d0e73eb65dc29ac3ddf75e892fec93aa343714ee
kalmarek <kalmar@amu.edu.pl>
2020-02-19 01:42:59 +0100

    arb_fac needs precision

--- src/arb.jl
+++ src/arb.jl
@@ -1,7 +1,7 @@
 @libcall arb_mul_2exp_si 2
 @libcall arb_one 1
 @libcall arb_pow_ui 2 prec=true
-@libcall arb_fac_ui 1
+@libcall arb_fac_ui 1 prec=true
 @libcall arb_div 3 prec=true
 @libcall arb_abs 2 prec=true
 @libcall arb_add_error 2
