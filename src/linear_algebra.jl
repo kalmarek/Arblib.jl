f013066f7fe570f00afa3dc69cbd56fbe94b2085
Sascha Timme <sascha@timme.xyz>
2020-08-20 10:31:31 +0200

    Add boundschecking for linear algebra routines

--- src/linear_algebra.jl
+++ src/linear_algebra.jl
@@ -1,14 +1,33 @@
-for (jf, af) in [(:+, :add!), (:-, :sub!), (:*, :mul!)]
+for (jf, af) in [(:+, :add!), (:-, :sub!)]
     @eval function Base.$jf(A::T, B::T) where {T<:Union{ArbMatrix,AcbMatrix}}
+        @boundscheck (
+            size(A) == size(B) ||
+            throw(DimensionMismatch("Matrix sizes are not compatible."))
+        )
         C = T(size(A, 1), size(B, 2); prec = max(precision(A), precision(B)))
         $af(C, A, B)
         C
     end
 end
+
 function Base.:(-)(A::T) where {T<:Union{ArbMatrix,AcbMatrix}}
     C = T(size(A)...; prec = precision(A))
     neg!(C, A)
     C
+end
+
+function LinearAlgebra.mul!(C::T, A::T, B::T) where {T<:Union{ArbMatrix,AcbMatrix}}
+    @boundscheck (
+        (size(C) == (size(A, 1), size(B, 2)) && size(A, 2) == size(B, 1)) ||
+        throw(DimensionMismatch("Matrix sizes are not compatible."))
+    )
+    Arblib.mul!(C, A, B)
+    C
+end
+
+function Base.:(*)(A::T, B::T) where {T<:Union{ArbMatrix,AcbMatrix}}
+    C = T(size(A, 1), size(B, 2); prec = max(precision(A), precision(B)))
+    LinearAlgebra.mul!(C, A, B)
 end
 
 function LinearAlgebra.lu!(A::T) where {T<:Union{ArbMatrix,AcbMatrix}}
@@ -29,15 +48,31 @@ function LinearAlgebra.inv(A::T) where {T<:Union{ArbMatrix,AcbMatrix}}
     B
 end
 
-LinearAlgebra.ldiv!(Y::T, A::T, B::T) where {T<:Union{ArbMatrix,AcbMatrix}} =
+function LinearAlgebra.ldiv!(Y::T, A::T, B::T) where {T<:Union{ArbMatrix,AcbMatrix}}
+    @boundscheck (
+        (size(Y) == size(B) && size(A, 1) == size(A, 2) && size(A, 1) == size(B, 1)) ||
+        throw(DimensionMismatch("Matrix sizes are not compatible."))
+    )
     LinearAlgebra.ldiv!(Y, LinearAlgebra.lu(A), B)
-LinearAlgebra.ldiv!(A::T, B::T) where {T<:Union{ArbMatrix,AcbMatrix}} =
+end
+
+function LinearAlgebra.ldiv!(A::T, B::T) where {T<:Union{ArbMatrix,AcbMatrix}}
+    @boundscheck (
+        (size(A, 1) == size(A, 2)) ||
+        throw(DimensionMismatch("Expected a square matrix as the left hand side."))
+    )
     LinearAlgebra.ldiv!(B, LinearAlgebra.lu(A), B)
+end
+
 function LinearAlgebra.ldiv!(
     Y::T,
     A::LinearAlgebra.LU{<:Any,T},
     B::T,
 ) where {T<:Union{ArbMatrix,AcbMatrix}}
+    @boundscheck (
+        (size(Y) == size(B) && size(A, 1) == size(A, 2) && size(A, 1) == size(B, 1)) ||
+        throw(DimensionMismatch("Matrix sizes are not compatible."))
+    )
     Arblib.solve_lu_precomp!(Y, A.ipiv, A.factors, B)
     Y
 end
--- test/linear_algebra.jl
+++ test/linear_algebra.jl
@@ -21,4 +21,13 @@
     id = copy(A)
     Arblib.one!(id)
     @test Bool(Arblib.contains(inv(A) * A, id))
+
+    # mul
+    A = ArbMatrix(rand(3,2))
+    B = ArbMatrix(rand(2,4))
+    B_wrong = ArbMatrix(rand(3,4))
+    C = ArbMatrix(3,4)
+    @test_throws DimensionMismatch("Matrix sizes are not compatible.") A * B_wrong
+    LinearAlgebra.mul!(C, A, B)
+    @test C == A * B
 end
