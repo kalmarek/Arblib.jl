bbac3eebc9f34bcae4ef98a075ba76886f827305
Sascha Timme <sascha@timme.xyz>
2020-08-20 10:37:31 +0200

    Also promote from Rational to Arb/Acb and restrict Complex slightly

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -1,8 +1,19 @@
 Base.promote_rule(::Type{Arf}, ::Type{<:Union{AbstractFloat,Integer}}) = Arf
-Base.promote_rule(::Type{Arb}, ::Type{<:Union{AbstractFloat,Integer,Arf,ArbRef}}) = Arb
+Base.promote_rule(::Type{Arb}, ::Type{<:Union{AbstractFloat,Integer,Rational,Arf,ArbRef}}) = Arb
 Base.promote_rule(
     ::Type{Acb},
-    ::Type{<:Union{AbstractFloat,Integer,Complex,Arf,Arb,ArbRef,AcbRef}},
+    ::Type{
+        <:Union{
+            AbstractFloat,
+            Integer,
+            Rational,
+            Complex{<:Union{AbstractFloat,Integer,Rational}},
+            Arf,
+            Arb,
+            ArbRef,
+            AcbRef,
+        },
+    },
 ) = Acb
 
 for (jf, af) in [(:+, :add!), (:-, :sub!), (:*, :mul!), (:/, :div!)]
