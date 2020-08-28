0152be2e7b5b271b65aab5e0836cb34e618faff1
Joel Dahne <joel@dahne.eu>
2020-08-28 17:23:35 +0200

    Move ArbPoly and ArbSeries constructors to poly.jl

--- src/poly.jl
+++ src/poly.jl
@@ -1,3 +1,4 @@
+# Interface
 Base.length(poly::Union{arb_poly_struct,ArbPoly}) =
     ccall(@libarb(arb_poly_length), Int, (Ref{arb_poly_struct},), poly)
 Base.length(series::ArbSeries) = series.degree + 1
@@ -25,3 +26,39 @@ Base.@propagate_inbounds function Base.setindex!(
     set_coeff!(poly, i, x)
     return x
 end
+
+# Constructors
+function ArbPoly(coeff::Arb; prec::Integer = precision(coeff))
+    poly = ArbPoly(prec = prec)
+    poly[0] = coeff
+    return poly
+end
+
+function ArbPoly(coeffs::AbstractVector{Arb}; prec::Integer = precision(first(coeffs)))
+    poly = ArbPoly(prec = prec)
+    @inbounds for i = 1:length(coeffs)
+        poly[i] = coeffs[i]
+    end
+    return poly
+end
+
+function ArbSeries(coeff::Arb, N::Integer; prec::Integer = precision(coeff))
+    series = ArbSeries(N, prec = prec)
+    series[0] = coeff
+    return series
+end
+
+function ArbSeries(coeffs::AbstractVector{Arb}; prec::Integer = precision(first(coeffs)))
+    series = ArbSeries(length(coeffs), prec = prec)
+    @inbounds for i = 1:length(coeffs)
+        series[i] = coeffs[i]
+    end
+    return series
+end
+
+Base.zero(poly::ArbPoly) = ArbPoly(prec = precision(poly))
+Base.one(poly::ArbPoly) =
+    ArbPoly([Arb(1, prec = precision(series))], prec = precision(poly))
+Base.zero(series::ArbSeries) = ArbSeries(degree(series), prec = precision(series))
+Base.one(series::ArbSeries) =
+    ArbSeries([Arb(1, prec = precision(series))]degree(series), prec = precision(series))
