ba8812e32858cab0216421e9c24921e636801bcf
Joel Dahne <joel@dahne.eu>
2020-09-17 17:58:48 +0200

    Add more logging to parse method

--- parse/parse.jl
+++ parse/parse.jl
@@ -47,23 +47,31 @@ For each function it checks that it is correctly parsed. Those that do
 not parse due to unsupported types are commented out in the code. If
 they do not parse for some other reason and error is thrown.
 """
-function generate_file(filename, title, sections)
-    str = generate_file(title, sections)
+function generate_file(filename, title, sections; verbose = false)
+    str = generate_file(title, sections, verbose = verbose)
     open(filename, "w") do file
         write(file, str)
     end
     return str
 end
 
-function generate_file(title, sections)
+function generate_file(title, sections; verbose = false)
     str = "###\n"
     str *= "### " * title * "\n"
     str *= "###\n"
+
+    num_functions = 0
+    num_unsupparg = 0
+    num_keyerror = 0
+
+    unsuppargs = Set{String}()
+    keyerrors = Set{String}()
 
     for (section_title, functions) in sections
         str *= "\n### " * section_title * "\n"
 
         for s in functions
+            num_functions += 1
             try
                 f = Arblib.Arbfunction(s)
 
@@ -73,8 +81,12 @@ function generate_file(title, sections)
                 str *= "arbcall\"" * s * "\"\n"
             catch e
                 if e isa Arblib.UnsupportedArgumentType
+                    push!(unsuppargs, e.key)
+                    num_unsupparg += 1
                     str *= "# arbcall\"" * s * "\"\n"
                 elseif e isa KeyError
+                    push!(keyerrors, e.key)
+                    num_keyerror += 1
                     str *= "# arbcall\"" * s * "\"\n"
                 else
                     rethrow(e)
@@ -82,5 +94,13 @@ function generate_file(title, sections)
             end
         end
     end
+
+    if verbose
+        correctly_parsed = num_functions - num_unsupparg - num_keyerror
+        @info "Correctly parsed functions: $correctly_parsed/$num_functions"
+        @info "Unsupported types: $(collect(unsuppargs))"
+        @info "Not implemented types: $(collect(keyerrors))"
+    end
+
     return str
 end
