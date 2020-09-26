39dcb834680700f913700e75a4a2929486faa5cd
Joel Dahne <joel@dahne.eu>
2020-09-26 10:57:57 +0200

    Add more tests for types and clean up old ones

--- test/types-test.jl
+++ test/types-test.jl
@@ -1,28 +1,48 @@
 @testset "types" begin
-    Mag = Arblib.Mag
-
     @testset "Mag" begin
-        @test typeof(Mag()) == Mag
-        @test typeof(Mag(Mag())) == Mag
-        @test typeof(Mag(Arf())) == Mag
+        @test Mag() isa Mag
+        @test Mag(Mag()) isa Mag
+        @test Mag(Arf()) isa Mag
     end
 
     @testset "Arf" begin
-        @test typeof(Arf()) == Arf
-        @test precision(Arf(prec = 64)) == 64
-        @test typeof(Arf(UInt64(1))) == Arf
-        @test precision(Arf(UInt64(1), prec = 64)) == 64
-        @test typeof(Arf(1)) == Arf
-        @test precision(Arf(1, prec = 64)) == 64
+        @test Arf() isa Arf
+        @test precision(Arf(prec = 80)) == 80
+        @test Arf(UInt64(1)) isa Arf
+        @test precision(Arf(UInt64(1), prec = 80)) == 80
+        @test Arf(1) isa Arf
+        @test precision(Arf(1, prec = 80)) == 80
     end
 
     @testset "Arb" begin
-        @test typeof(Arb()) == Arb
-        @test precision(Arb(prec = 64)) == 64
+        @test Arb() isa Arb
+        @test precision(Arb(prec = 80)) == 80
     end
 
     @testset "Acb" begin
-        @test typeof(Acb()) == Acb
-        @test precision(Acb(prec = 64)) == 64
+        @test Acb() isa Acb
+        @test precision(Acb(prec = 80)) == 80
+    end
+
+    @testset "ArbPoly" begin
+        @test ArbPoly() isa ArbPoly
+        @test precision(ArbPoly(prec = 80)) == 80
+    end
+
+    @testset "ArbSeries" begin
+        @test ArbSeries(3) isa ArbSeries
+        @test precision(ArbSeries(3, prec = 80)) == 80
+        @test ArbSeries(3).degree == 3
+    end
+
+    @testset "AcbPoly" begin
+        @test AcbPoly() isa AcbPoly
+        @test precision(AcbPoly(prec = 80)) == 80
+    end
+
+    @testset "AcbSeries" begin
+        @test AcbSeries(3) isa AcbSeries
+        @test precision(AcbSeries(3, prec = 80)) == 80
+        @test AcbSeries(3).degree == 3
     end
 end
