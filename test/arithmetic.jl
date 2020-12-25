82c676386f90a0fbd95e60e04220c15af9c7d3f9
Joel Dahne <joel@dahne.eu>
2020-12-25 16:42:45 +0100

    Cleanup tests for arithmetic and constructors
    Deletes some and moves some of the tests in arithmetic that no longer
    belong there.

--- test/arithmetic.jl
+++ test/arithmetic.jl
@@ -67,7 +67,7 @@
         @test Arblib.root(Arf(8), 3) == Arf(2)
     end
 
-    @testset "basic arithmetic: $T" for T in [Arb, Acb]
+    @testset "$T" for T in [Arb, Acb]
         @test T(3) + 1 == 4
         @test T(3) + 1 isa T
         @test T(2) + T(3) == 5
@@ -84,53 +84,15 @@
         @test T(3) / T(1) isa T
     end
 
-    @testset "promote: $T" for (T, TMat) in [(Arb, ArbMatrix), (Acb, AcbMatrix)]
-        A = TMat(3, 3)
-        # promotion of TRef to T
-        a = A[1, 1]
-        promote(a, 1) isa Tuple{T,T}
-        promote(a, T(1)) isa Tuple{T,T}
-        @test a + 3 == 3
-        @test a + 3 isa T
-    end
+    @testset "Acb - specific" begin
+        @test real(Acb(1, 2)) isa Arb
+        @test real(Acb(1, 2)) == Arb(1)
+        @test imag(Acb(1, 2)) isa Arb
+        @test imag(Acb(1, 2)) == Arb(2)
 
-    @testset "real/imag" begin
-        x, y = Arb(rand()), Arb(rand())
-        z = Acb(x, y)
+        @test conj(Acb(1, 2)) == Acb(1, -2)
 
-        @test Arblib.realref(z) isa ArbRef
-        @test Arblib.realref(z) == x
-        @test real(z) == x
-        @test Arblib.imagref(z) isa ArbRef
-        @test Arblib.imagref(z) == y
-        @test imag(z) == y
-    end
-
-    @testset "midref" begin
-        x = Arb(0.25)
-        @test Arblib.midref(x) isa ArfRef
-        @test startswith(sprint(show, x), "0.250")
-        @test Float64(Arblib.midref(x)) isa Float64
-        @test Float64(Arblib.midref(x)) == 0.25
-        @test Float64(x) == 0.25
-        @test sprint(show, x) == sprint(show, x[])
-    end
-
-    @testset "radref" begin
-        x = Arb(0.25)
-        m = Arblib.radref(x)
-        @test m isa MagRef
-        m[] = 1.0
-        @test Float64(m) ≥ 1.0
-        @test sprint(show, m) == sprint(show, m[])
-    end
-
-    @testset "convert to Float64/ComplexF64" begin
-        x = Arb(0.25)
-        @test Float64(x) isa Float64
-        @test Float64(x) == 0.25
-        z = Acb(2.0 + 0.125im)
-        @test ComplexF64(z) isa ComplexF64
-        @test ComplexF64(z) == 2.0 + 0.125im
+        @test abs(Acb(3, 4)) isa Arb
+        @test abs(Acb(3, 4)) == Arb(5)
     end
 end
