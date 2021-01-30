c691c2ad05dc0c54297a8e5d52eba9effb7a2b86
Joel Dahne <joel@dahne.eu>
2021-01-30 20:52:42 +0100

    Add inplace version of eps and throw error for special values

--- src/float.jl
+++ src/float.jl
@@ -1,6 +1,20 @@
-Base.eps(T::Type{<:Union{ArfOrRef,ArbOrRef,AcbOrRef}}) = eps(one(T))
-Base.eps(x::ArfOrRef) = Arf(set_ulp!(Mag(), x, prec = precision(x)), prec = precision(x))
-Base.eps(x::ArbOrRef) =
-    Arb(set_ulp!(Mag(), midref(x), prec = precision(x)), prec = precision(x))
-Base.eps(x::AcbOrRef) =
-    Acb(set_ulp!(Mag(), midref(realref(x)), prec = precision(x)), prec = precision(x))
+function eps!(res::ArfOrRef, x::ArfOrRef)
+    # FIXME: Should we return NaN here?
+    isspecial(x) && throw(ArgumentError("eps not defined for special values, got $x"))
+    return set!(res, set_ulp!(Mag(), x, prec = precision(x)))
+end
+
+function eps!(res::ArbOrRef, x::ArbOrRef)
+    # FIXME: Should we return NaN here?
+    mid_x = midref(x)
+    isspecial(mid_x) && throw(ArgumentError("eps not defined for special values, got $x"))
+    rad_res = radref(res)
+    set_ulp!(rad_res, mid_x, prec = precision(x))
+    return set!(res, rad_res)
+end
+
+function Base.eps(T::Type{<:Union{ArfOrRef,ArbOrRef}})
+    res = one(T)
+    return eps!(res, res)
+end
+Base.eps(x::Union{ArfOrRef,ArbOrRef}) = eps!(zero(x), x)
--- test/float.jl
+++ test/float.jl
@@ -6,18 +6,28 @@
         @test Arf(4) + eps(Arf(4)) / 2 == Arf(4)
         @test Arf(4, prec = 64) + eps(Arf(4, prec = 64)) > Arf(4)
         @test Arf(4, prec = 64) + eps(Arf(4, prec = 64)) / 2 == Arf(4)
+
+        @test eps(Arf) == Arblib.eps!(zero(Arf), one(Arf))
+        @test eps(Arf(4)) == Arblib.eps!(one(Arf), Arf(4))
+        @test eps(Arb) == Arblib.eps!(zero(Arb), one(Arb))
+        @test eps(Arb(4)) == Arblib.eps!(one(Arb), Arb(4))
+
+        @test eps(Arb) == Arb(eps(Arf))
+
         @test eps(Arf) isa Arf
         @test eps(ArfRef) isa Arf
         @test eps(one(Arf)) isa Arf
-
-        @test eps(Arb) == Arb(eps(Arf))
         @test eps(Arb) isa Arb
         @test eps(ArbRef) isa Arb
         @test eps(one(Arb)) isa Arb
 
-        @test eps(Acb) == Acb(eps(Arf))
-        @test eps(Acb) isa Acb
-        @test eps(AcbRef) isa Acb
-        @test eps(one(Acb)) isa Acb
+        @test precision(eps(Arf(1, prec = 64))) == 64
+        @test precision(eps(Arb(1, prec = 64))) == 64
+
+        # Test aliasing
+        x = Arf(4)
+        @test Arblib.eps!(x, x) == eps(Arf(4))
+        x = Arb(4)
+        @test Arblib.eps!(x, x) == eps(Arb(4))
     end
 end
