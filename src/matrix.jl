dce886040c45923416db55780dc4305a9969ce8e
nanleij <38724800+nanleij@users.noreply.github.com>
2021-05-15 12:04:10 +0200

    Scalar multiplication and scalar division
    I am not totally sure about Acb * ArbMatrix and Acb \ ArbMatrix

--- src/matrix.jl
+++ src/matrix.jl
@@ -168,6 +168,41 @@ function Base.:(*)(A::T, B::T) where {T<:Matrices}
     LinearAlgebra.mul!(C, A, B)
 end
 
+# scalar multiplication
+function Base.:(*)(c::ArbLike, A::T) where {T <: Matrices}
+    C = T(size(A,1),size(A,2),prec = precision(A))
+    Arblib.mul!(C,A,c)
+end
+function Base.:(*)(c::AcbLike, A::AcbMatLike)
+    C = similar(A)
+    Arblib.mul!(C,A,c)
+end
+function Base.:(*)(c::AcbLike, A::ArbMatrixLike)
+    C = AcbMatrix(A)
+    Arblib.mul!(C,AcbMatrix(A),c)
+end
+Base.:(*)(A::T, c::AcbLike) = c*A
+Base.:(*)(A::T, c::ArbLike) = c*A
+
+
+
+# scalar division
+function Base.:(\)(c::ArbLike, A::T) where {T <: Matrices}
+    C = T(size(A,1),size(A,2),prec = precision(A))
+    Arblib.div!(C,A,c)
+end
+function Base.:(\)(c::AcbLike, A::AcbMatrixLike)
+    C = similar(A)
+    Arblib.div!(C,A,c)
+end
+function Base.:(\)(c::AcbLike, A::ArbMatrixLike)
+    C = AcbMatrix(A)
+    Arblib.div!(C,AcbMatrix(A),c)
+end
+Base.:(/)(A::T, c::AcbLike) = c\A
+Base.:(/)(A::T, c::ArbLike) = c\A
+
+
 # lu factorization
 function LinearAlgebra.lu!(A::T) where {T<:Matrices}
     ipiv = zeros(Int, size(A, 2))
