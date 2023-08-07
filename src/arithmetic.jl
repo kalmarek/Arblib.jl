a06d9f842fd15a07e16687a172a67a801c83ed98
Joel Dahne <joel@dahne.eu>
2023-08-07 14:02:48 +0200

    Cleanup of arithmethic.jl and a few new methods
    Adds the following new methods:
    - Multi argument versions of + and *
    - fma and muladd
    - cube
    - signbit
    
    It also adds a special case for binary operations with irrationals,
    reducing the number of allocations.

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -2,44 +2,79 @@ const _BitInteger = (Int == Int64 ? Base.BitInteger64 : Base.BitInteger32)
 const _BitSigned = (Int == Int64 ? Base.BitSigned64 : Base.BitSigned32)
 const _BitUnsigned = (Int == Int64 ? Base.BitUnsigned64 : Base.BitUnsigned32)
 
-### Mag
+### +, -, *, /
 for (jf, af) in [(:+, :add!), (:-, :sub!), (:*, :mul!), (:/, :div!)]
     @eval Base.$jf(x::MagOrRef, y::MagOrRef) = $af(zero(x), x, y)
-end
-Base.:+(x::MagOrRef, y::Integer) = add!(zero(x), x, convert(UInt, y))
-Base.:+(x::Integer, y::MagOrRef) = add!(zero(y), y, convert(UInt, x))
-Base.:*(x::MagOrRef, y::Integer) = mul!(zero(x), x, convert(UInt, y))
-Base.:*(x::Integer, y::MagOrRef) = mul!(zero(y), y, convert(UInt, x))
-Base.:/(x::MagOrRef, y::Integer) = div!(zero(x), x, convert(UInt, y))
 
-Base.:(^)(x::MagOrRef, e::Integer) = pow!(zero(x), x, convert(UInt, e))
-Base.inv(x::MagOrRef) = inv!(zero(x), x)
-
-### Arf
-function Base.sign(x::ArfOrRef)
-    isnan(x) && return Arf(NaN) # Follow Julia and return NaN
-    return Arf(sgn(x))
-end
-
-Base.abs(x::ArfOrRef) = abs!(zero(x), x)
-Base.:(-)(x::ArfOrRef) = neg!(zero(x), x)
-for (jf, af) in [(:+, :add!), (:-, :sub!), (:*, :mul!), (:/, :div!)]
     @eval function Base.$jf(x::ArfOrRef, y::Union{ArfOrRef,_BitInteger})
         z = Arf(prec = _precision(x, y))
         $af(z, x, y)
         return z
     end
+
+    @eval Base.$jf(x::ArbOrRef, y::Union{ArbOrRef,ArfOrRef,_BitInteger}) =
+        $af(Arb(prec = _precision(x, y)), x, y)
+
+    @eval Base.$jf(x::AcbOrRef, y::Union{AcbOrRef,ArbOrRef,_BitInteger}) =
+        $af(Acb(prec = _precision(x, y)), x, y)
+
+    # Avoid one allocation for operations on irrationals
+    @eval function Base.$jf(x::Union{ArbOrRef,AcbOrRef}, y::Irrational)
+        z = zero(x)
+        z[] = y
+        return $af(z, x, z)
+    end
+
+    if jf == :+ || jf == :*
+        # Addition and multiplication is commutative
+        @eval Base.$jf(x::_BitInteger, y::ArfOrRef) = $jf(y, x)
+
+        @eval Base.$jf(x::Union{ArfOrRef,_BitInteger,Irrational}, y::ArbOrRef) = $jf(y, x)
+
+        @eval Base.$jf(x::Union{ArbOrRef,_BitInteger,Irrational}, y::AcbOrRef) = $jf(y, x)
+
+        # Define more efficient multi argument versions, similar to BigFloat
+        # TODO: Now precision is determined by first two arguments
+        for T in (MagOrRef, ArfOrRef, ArbOrRef, AcbOrRef)
+            @eval function Base.$jf(a::$T, b::$T, c::$T)
+                z = $jf(a, b)
+                $af(z, z, c)
+                return z
+            end
+
+            @eval function Base.$jf(a::$T, b::$T, c::$T, d::$T)
+                z = $jf(a, b)
+                $af(z, z, c)
+                $af(z, z, d)
+                return z
+            end
+
+            @eval function Base.$jf(a::$T, b::$T, c::$T, d::$T, e::$T)
+                z = $jf(a, b)
+                $af(z, z, c)
+                $af(z, z, d)
+                $af(z, z, e)
+                return z
+            end
+        end
+    else
+        @eval function Base.$jf(x::Irrational, y::Union{ArbOrRef,AcbOrRef})
+            z = zero(y)
+            z[] = x
+            return $af(z, z, y)
+        end
+    end
 end
-function Base.:+(x::_BitInteger, y::ArfOrRef)
-    z = zero(y)
-    add!(z, y, x)
-    return z
-end
-function Base.:*(x::_BitInteger, y::ArfOrRef)
-    z = zero(y)
-    mul!(z, y, x)
-    return z
-end
+
+Base.:-(x::Union{ArfOrRef,ArbOrRef,AcbOrRef}) = neg!(zero(x), x)
+Base.inv(x::Union{MagOrRef,ArbOrRef,AcbOrRef}) = inv!(zero(x), x)
+
+Base.:+(x::MagOrRef, y::Integer) = add!(zero(x), x, convert(UInt, y))
+Base.:+(x::Integer, y::MagOrRef) = y + x
+Base.:*(x::MagOrRef, y::Integer) = mul!(zero(x), x, convert(UInt, y))
+Base.:*(x::Integer, y::MagOrRef) = y * x
+Base.:/(x::MagOrRef, y::Integer) = div!(zero(x), x, convert(UInt, y))
+
 function Base.:/(x::_BitUnsigned, y::ArfOrRef)
     z = zero(y)
     ui_div!(z, x, y)
@@ -51,47 +86,9 @@ function Base.:/(x::_BitSigned, y::ArfOrRef)
     return z
 end
 
-### Arb and Acb
-for (jf, af) in [(:+, :add!), (:-, :sub!), (:*, :mul!), (:/, :div!)]
-    @eval Base.$jf(x::ArbOrRef, y::Union{ArbOrRef,ArfOrRef,_BitInteger}) =
-        $af(Arb(prec = _precision(x, y)), x, y)
-    @eval Base.$jf(x::AcbOrRef, y::Union{AcbOrRef,ArbOrRef,_BitInteger}) =
-        $af(Acb(prec = _precision(x, y)), x, y)
-    if jf == :(+) || jf == :(*)
-        @eval Base.$jf(x::Union{ArfOrRef,_BitInteger}, y::ArbOrRef) =
-            $af(Arb(prec = _precision(x, y)), y, x)
-        @eval Base.$jf(x::Union{ArbOrRef,_BitInteger}, y::AcbOrRef) =
-            $af(Acb(prec = _precision(x, y)), y, x)
-    end
-end
-
-Base.:(-)(x::Union{ArbOrRef,AcbOrRef}) = neg!(zero(x), x)
-Base.abs(x::ArbOrRef) = abs!(zero(x), x)
 Base.:(/)(x::_BitUnsigned, y::ArbOrRef) = ui_div!(zero(y), x, y)
 
-Base.:(^)(x::ArbOrRef, y::ArbOrRef) = pow!(Arb(prec = _precision(x, y)), x, y)
-function Base.:(^)(x::ArbOrRef, y::_BitInteger)
-    z = zero(x)
-    x, n = (y >= 0 ? (x, y) : (inv!(z, x), -y))
-    return pow!(z, x, convert(UInt, n))
-end
-Base.:(^)(x::AcbOrRef, y::Union{AcbOrRef,ArbOrRef,_BitInteger}) =
-    pow!(Acb(prec = _precision(x, y)), x, y)
-
-# We define the same special cases as Arb does, this avoids some
-# overhead
-Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{-2}) =
-    (y = inv(x); sqr!(y, y))
-#Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{-1}) - implemented in Base.intfuncs.jl
-Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{0}) = one(x)
-Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{1}) = copy(x)
-Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{2}) = sqr(x)
-
-sqr(x::Union{ArbOrRef,AcbOrRef}) = sqr!(zero(x), x)
-Base.inv(x::Union{ArbOrRef,AcbOrRef}) = inv!(zero(x), x)
-
-### Acb
-function Base.:(*)(x::AcbOrRef, y::Complex{Bool})
+function Base.:*(x::AcbOrRef, y::Complex{Bool})
     if real(y)
         if imag(y)
             z = mul_onei!(zero(x), x)
@@ -103,9 +100,64 @@ function Base.:(*)(x::AcbOrRef, y::Complex{Bool})
     imag(y) && return mul_onei!(zero(x), x)
     return zero(x)
 end
-Base.:(*)(x::Complex{Bool}, y::AcbOrRef) = y * x
+Base.:*(x::Complex{Bool}, y::AcbOrRef) = y * x
 
-Base.real(z::AcbLike; prec = _precision(z)) = get_real!(Arb(; prec), z)
-Base.imag(z::AcbLike; prec = _precision(z)) = get_imag!(Arb(; prec), z)
-Base.conj(z::AcbLike) = conj!(Acb(prec = _precision(z)), z)
-Base.abs(z::AcbLike) = abs!(Arb(prec = _precision(z)), z)
+### fma and muladd
+function Base.fma(x::ArfOrRef, y::ArfOrRef, z::ArfOrRef)
+    res = zero(x)
+    fma!(res, x, y, z)
+    return res
+end
+Base.fma(x::ArbOrRef, y::ArbOrRef, z::ArbOrRef) = fma!(zero(x), x, y, z)
+Base.fma(x::ArbOrRef, y::Union{_BitInteger,ArfOrRef}, z::ArbOrRef) = fma!(zero(x), x, y, z)
+Base.fma(x::Union{_BitInteger,ArfOrRef}, y::ArbOrRef, z::ArbOrRef) = fma(y, x, z)
+
+Base.muladd(x::ArfOrRef, y::ArfOrRef, z::ArfOrRef) = fma(x, y, z)
+Base.muladd(x::ArbOrRef, y::ArbOrRef, z::ArbOrRef) = fma(x, y, z)
+Base.muladd(x::ArbOrRef, y::Union{_BitInteger,ArfOrRef}, z::ArbOrRef) = fma(x, y, z)
+Base.muladd(x::Union{_BitInteger,ArfOrRef}, y::ArbOrRef, z::ArbOrRef) = fma(x, y, z)
+
+### signbit, sign and abs
+Base.signbit(x::MagOrRef) = false
+Base.signbit(x::ArfOrRef) = !isnan(x) && sgn(x) < 0
+Base.signbit(x::ArbOrRef) = isnegative(x)
+
+# For Arf sign of NaN is undefined, we follow Julia and return NaN
+Base.sign(x::MagOrRef) = iszero(x) ? zero(x) : one(x)
+Base.sign(x::ArfOrRef) = isnan(x) ? nan!(zero(x)) : Arf(sgn(x), prec = _precision(x))
+Base.sign(x::Union{ArbOrRef,AcbRef}) = sgn!(zero(x), x)
+
+Base.abs(x::MagOrRef) = copy(x)
+Base.abs(x::Union{ArfOrRef,ArbOrRef}) = abs!(zero(x), x)
+Base.abs(z::AcbOrRef) = abs!(Arb(prec = _precision(z)), z)
+
+### ^
+
+Base.:^(x::MagOrRef, e::Integer) = pow!(zero(x), x, convert(UInt, e))
+Base.:^(x::ArbOrRef, y::ArbOrRef) = pow!(Arb(prec = _precision(x, y)), x, y)
+function Base.:^(x::ArbOrRef, y::_BitInteger)
+    z = zero(x)
+    x, n = (y >= 0 ? (x, y) : (inv!(z, x), -y))
+    return pow!(z, x, convert(UInt, n))
+end
+Base.:^(x::AcbOrRef, y::Union{AcbOrRef,ArbOrRef,_BitInteger}) =
+    pow!(Acb(prec = _precision(x, y)), x, y)
+
+sqr(x::Union{ArbOrRef,AcbOrRef}) = sqr!(zero(x), x)
+cube(x::AcbOrRef) = cube!(zero(x), x)
+
+# We define the same special cases as Arb does, this avoids some overhead
+Base.literal_pow(::typeof(^), x::AcbOrRef, ::Val{-3}) = (y = inv(x); cube!(y, y))
+Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{-2}) =
+    (y = inv(x); sqr!(y, y))
+#Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{-1}) - implemented in Base.intfuncs.jl
+Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{0}) = one(x)
+Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{1}) = copy(x)
+Base.literal_pow(::typeof(^), x::Union{ArbOrRef,AcbOrRef}, ::Val{2}) = sqr(x)
+Base.literal_pow(::typeof(^), x::AcbOrRef, ::Val{3}) = cube(x)
+
+### real, imag, conj
+
+Base.real(z::AcbOrRef) = get_real!(Arb(prec = _precision(z)), z)
+Base.imag(z::AcbOrRef) = get_imag!(Arb(prec = _precision(z)), z)
+Base.conj(z::AcbOrRef) = conj!(zero(z), z)
--- test/arithmetic.jl
+++ test/arithmetic.jl
@@ -1,5 +1,6 @@
 @testset "Arithmethic" begin
     @testset "Mag" begin
+        # +, -, *, /
         @test Mag(4) <= Mag(1) + Mag(3) <= Mag(5)
         @test Mag(2) <= Mag(3) - Mag(1) <= Mag(3)
         @test Mag(0) <= Mag(3) - Mag(5) <= Mag(1)
@@ -11,16 +12,31 @@
         @test Mag(2) <= Mag(4) / 2 <= Mag(3)
 
         @test Mag(1 / 4) <= inv(Mag(4)) <= Mag(1 / 3)
+
+        @test Mag(6) <= +(Mag.((1, 2, 3))...) <= Mag(7)
+        @test Mag(10) <= +(Mag.((1, 2, 3, 4))...) <= Mag(11)
+        @test Mag(15) <= +(Mag.((1, 2, 3, 4, 5))...) <= Mag(16)
+        @test Mag(6) <= *(Mag.((1, 2, 3))...) <= Mag(7)
+        @test Mag(24) <= *(Mag.((1, 2, 3, 4))...) <= Mag(25)
+        @test Mag(120) <= *(Mag.((1, 2, 3, 4, 5))...) <= Mag(121)
+
+        # signbit, sign and abs
+        @test !signbit(Mag(0))
+        @test !signbit(Mag(2))
+
+        @test iszero(sign(Mag(0)))
+        @test isone(sign(Mag(1)))
+        @test isone(sign(Mag(Inf)))
+
+        @test abs(Mag(5)) == Mag(5)
+
+        # ^
         @test Mag(8) <= Mag(2)^3 <= Mag(9)
     end
 
     @testset "Arf" begin
-        @test sign(Arf(2)) == Arf(1)
-        @test sign(Arf(-2)) == Arf(-1)
-        @test sign(Arf(0)) == Arf(0)
-
-        @test abs(Arf(-2)) == abs(Arf(2)) == Arf(2)
-        @test -Arf(2) == Arf(-2)
+        # +, -, *, /
+        @test -Arf(2) == -2
 
         @test Arf(1) + Arf(2) ==
               Arf(1) + 2 ==
@@ -30,14 +46,14 @@
               Arf(1) + UInt8(2) ==
               Arf(1) + UInt8(2) ==
               UInt8(2) + Arf(1) ==
-              Arf(3)
-        @test Arf(1) - Arf(2) == Arf(1) - 2 == Arf(1) - UInt(2) == Arf(-1)
+              3
+        @test Arf(1) - Arf(2) == Arf(1) - 2 == Arf(1) - UInt(2) == -1
         @test Arf(2) * Arf(3) ==
               Arf(2) * 3 ==
               3 * Arf(2) ==
               Arf(2) * UInt(3) ==
               UInt(3) * Arf(2) ==
-              Arf(6)
+              6
         @test Arf(6) / Arf(2) ==
               Arf(6) / 2 ==
               6 / Arf(2) ==
@@ -45,7 +61,30 @@
               UInt(6) / Arf(2) ==
               Arf(6) / UInt8(2) ==
               UInt8(6) / Arf(2) ==
-              Arf(3)
+              3
+
+        @test +(Arf.((1, 2, 3))...) == 6
+        @test +(Arf.((1, 2, 3, 4))...) == 10
+        @test +(Arf.((1, 2, 3, 4, 5))...) == 15
+        @test *(Arf.((1, 2, 3))...) == 6
+        @test *(Arf.((1, 2, 3, 4))...) == 24
+        @test *(Arf.((1, 2, 3, 4, 5))...) == 120
+
+        # fma and muladd
+        @test fma(Arf(2), Arf(3), Arf(4)) == muladd(Arf(2), Arf(3), Arf(4)) == 10
+
+        # signbit, sign and abs
+        @test signbit(Arf(-2))
+        @test !signbit(Arf(0))
+        @test !signbit(Arf(2))
+        @test !signbit(Arf(NaN))
+
+        @test sign(Arf(-2)) == -1
+        @test sign(Arf(0)) == 0
+        @test sign(Arf(2)) == 1
+        @test sign(Arf(NaN)) == NaN
+
+        @test abs(Arf(-2)) == abs(Arf(2)) == 2
     end
 
     @testset "$T" for T in [Arb, Acb]
@@ -56,57 +95,74 @@
               UInt(2) + T(1) ==
               T(1) + UInt8(2) ==
               UInt8(2) + T(1) ==
-              T(3)
+              3
         @test T(1) - T(2) == T(1) - 2 == T(1) - UInt(2) == T(-1)
-        @test T(2) * T(3) ==
-              T(2) * 3 ==
-              3 * T(2) ==
-              T(2) * UInt(3) ==
-              UInt(3) * T(2) ==
-              T(6)
-        @test T(6) / T(2) == T(6) / 2 == T(6) / UInt(2) == T(3)
+        @test T(2) * T(3) == T(2) * 3 == 3 * T(2) == T(2) * UInt(3) == UInt(3) * T(2) == 6
+        @test T(6) / T(2) == T(6) / 2 == T(6) / UInt(2) == 3
 
+        @test +(T.((1, 2, 3))...) == 6
+        @test +(T.((1, 2, 3, 4))...) == 10
+        @test +(T.((1, 2, 3, 4, 5))...) == 15
+        @test *(T.((1, 2, 3))...) == 6
+        @test *(T.((1, 2, 3, 4))...) == 24
+        @test *(T.((1, 2, 3, 4, 5))...) == 120
+
+        # ^
         @test Base.literal_pow(^, T(2), Val(-2)) ==
               T(2)^-2 ==
               Arblib.sqr(inv(T(2))) ==
-              T(1 // 4)
-        @test Base.literal_pow(^, T(2), Val(0)) == T(2)^0 == one(T)
-        @test Base.literal_pow(^, T(2), Val(1)) == T(2)^1 == T(2)
-        @test Base.literal_pow(^, T(2), Val(2)) == T(2)^2 == Arblib.sqr(T(2)) == T(4)
-
-        @test inv(T(2)) == 1 // 2
-        @test Arblib.sqr(T(2)) == 4
+              1 // 4
+        @test Base.literal_pow(^, T(2), Val(-1)) == T(2)^-1 == inv(T(2)) == 1 // 2
+        @test Base.literal_pow(^, T(2), Val(0)) == T(2)^0 == 1
+        @test Base.literal_pow(^, T(2), Val(1)) == T(2)^1 == 2
+        @test Base.literal_pow(^, T(2), Val(2)) == T(2)^2 == Arblib.sqr(T(2)) == 4
     end
 
     @testset "Arb - specific" begin
-        @test Arb(1) + Arf(2) == Arf(2) + Arb(1) == Arb(3)
+        @test Arb(1) + Arf(2) == Arf(2) + Arb(1) == 3
         @test Arb(1) - Arf(2) == Arb(-1)
-        @test Arb(2) * Arf(3) == Arf(3) * Arb(2) == Arb(6)
-        @test Arb(6) / Arf(2) == UInt(6) / Arb(2) == UInt8(6) / Arb(2) == Arb(3)
+        @test Arb(2) * Arf(3) == Arf(3) * Arb(2) == 6
+        @test Arb(6) / Arf(2) == UInt(6) / Arb(2) == UInt8(6) / Arb(2) == 3
 
+        # fma and muladd
+        @test fma(Arb(2), Arb(3), Arb(4)) ==
+              fma(Arb(2), 3, Arb(4)) ==
+              fma(2, Arb(3), Arb(4)) ==
+              muladd(Arb(2), Arb(3), Arb(4)) ==
+              muladd(Arb(2), 3, Arb(4)) ==
+              muladd(2, Arb(3), Arb(4)) ==
+              10
+
+        # signbit, sign and abs
+        @test signbit(Arb(-2))
+        @test !signbit(Arb(0))
+        @test !signbit(Arb(2))
+        @test !signbit(Arb((-1, 0)))
+        @test !signbit(Arb(NaN))
+
+        @test sign(Arb(-2)) == -1
+        @test sign(Arb(0)) == 0
+        @test sign(Arb(2)) == 1
+        @test isequal(sign(Arb((-3, 1))), Arblib.zero_pm_one!(Arb()))
+        @test isequal(sign(Arb(NaN)), Arblib.zero_pm_one!(Arb()))
+
+        @test abs(Arb(-2)) == abs(Arb(2)) == 2
+
+        # ^
         @test Arb(2)^Arb(3) ==
               Arb(2)^3 ==
               Arb(2)^Int8(3) ==
               Arb(2)^UInt(3) ==
               Arb(2)^UInt8(3) ==
               (Arb(2)^-3)^-1 ==
-              Arb(8)
+              8
     end
 
     @testset "Acb - specific" begin
-        @test Acb(1) + Arb(2) == Arb(2) + Acb(1) == Acb(3)
-        @test Acb(1) - Arb(2) == Acb(-1)
-        @test Acb(2) * Arb(3) == Arb(3) * Acb(2) == Acb(6)
-        @test Acb(6) / Arb(2) == Acb(3)
-
-        @test Acb(2)^Acb(3) ==
-              Acb(2)^Arb(3) ==
-              Acb(2)^3 ==
-              Acb(2)^Int8(3) ==
-              Acb(2)^UInt(3) ==
-              Acb(2)^UInt8(3) ==
-              (Acb(2)^-3)^-1 ==
-              Acb(8)
+        @test Acb(1) + Arb(2) == Arb(2) + Acb(1) == 3
+        @test Acb(1) - Arb(2) == -1
+        @test Acb(2) * Arb(3) == Arb(3) * Acb(2) == 6
+        @test Acb(6) / Arb(2) == 3
 
         @test Acb(2, 3) * Complex{Bool}(0, 0) ==
               Complex{Bool}(0, 0) * Acb(2, 3) ==
@@ -121,14 +177,32 @@
               Complex{Bool}(1, 1) * Acb(2, 3) ==
               Acb(2, 3) * (1 + 1im)
 
+        # abs
+        @test abs(Acb(3, 4)) isa Arb
+        @test abs(Acb(3, 4)) == 5
+
+        # ^
+        @test Acb(2)^Acb(3) ==
+              Acb(2)^Arb(3) ==
+              Acb(2)^3 ==
+              Acb(2)^Int8(3) ==
+              Acb(2)^UInt(3) ==
+              Acb(2)^UInt8(3) ==
+              (Acb(2)^-3)^-1 ==
+              8
+
+        @test Base.literal_pow(^, Acb(2), Val(-3)) ==
+              Acb(2)^-3 ==
+              Arblib.cube(inv(Acb(2))) ==
+              1 // 8
+        @test Base.literal_pow(^, Acb(2), Val(3)) == Acb(2)^3 == Arblib.cube(Acb(2)) == 8
+
+        # real, imag, conj
         @test real(Acb(1, 2)) isa Arb
-        @test real(Acb(1, 2)) == Arb(1)
+        @test real(Acb(1, 2)) == 1
         @test imag(Acb(1, 2)) isa Arb
-        @test imag(Acb(1, 2)) == Arb(2)
+        @test imag(Acb(1, 2)) == 2
 
         @test conj(Acb(1, 2)) == Acb(1, -2)
-
-        @test abs(Acb(3, 4)) isa Arb
-        @test abs(Acb(3, 4)) == Arb(5)
     end
 end
