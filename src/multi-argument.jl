2b142de72ad9777540c976f0bfb70688c3c8de7a
Joel Dahne <joel@dahne.eu>
2023-10-08 19:28:53 +0200

    Add multi-argument + and * for polynomials

--- src/multi-argument.jl
+++ src/multi-argument.jl
@@ -29,13 +29,17 @@ for (jf, af) in [(:+, :add!), (:*, :mul!), (:min, :min!), (:max, :max!)]
     end
 end
 
-for T in (ArfOrRef, ArbOrRef, AcbOrRef)
+for T in (ArfOrRef, ArbOrRef, AcbOrRef, ArbPoly, AcbPoly)
     @eval @inline _precision(x::$T, y::$T, z::$T, rest::Vararg{S}) where {S<:$T} =
         max(precision(x), _precision(y, z, rest...))
 
     for (jf, af) in [(:+, :add!), (:*, :mul!), (:min, :min!), (:max, :max!)]
         T == AcbOrRef && jf == :min && continue
         T == AcbOrRef && jf == :max && continue
+        T == ArbPoly && jf == :min && continue
+        T == ArbPoly && jf == :max && continue
+        T == AcbPoly && jf == :min && continue
+        T == AcbPoly && jf == :max && continue
 
         @eval function Base.$jf(a::$T, b::$T, c::$T)
             res = $(_nonreftype(T))(prec = _precision(a, b, c))
@@ -58,6 +62,42 @@ for T in (ArfOrRef, ArbOrRef, AcbOrRef)
             $af(res, res, c)
             $af(res, res, d)
             $af(res, res, e)
+            return res
+        end
+    end
+end
+
+for T in (ArbSeries, AcbSeries)
+    @eval @inline _precision(x::$T, y::$T, z::$T, rest::Vararg{S}) where {S<:$T} =
+        max(precision(x), _precision(y, z, rest...))
+    @eval @inline _degree(x::$T, y::$T, z::$T, rest::Vararg{S}) where {S<:$T} =
+        min(degree(x), _degree(y, z, rest...))
+
+    for (jf, af) in [(:+, :add_series!), (:*, :mullow!)]
+        @eval function Base.$jf(a::$T, b::$T, c::$T)
+            deg = _degree(a, b, c)
+            res = $T(degree = deg, prec = _precision(a, b, c))
+            $af(res, a, b, deg + 1)
+            $af(res, res, c, deg + 1)
+            return res
+        end
+
+        @eval function Base.$jf(a::$T, b::$T, c::$T, d::$T)
+            deg = _degree(a, b, c, d)
+            res = $T(degree = deg, prec = _precision(a, b, c, d))
+            $af(res, a, b, deg + 1)
+            $af(res, res, c, deg + 1)
+            $af(res, res, d, deg + 1)
+            return res
+        end
+
+        @eval function Base.$jf(a::$T, b::$T, c::$T, d::$T, e::$T)
+            deg = _degree(a, b, c, d)
+            res = $T(degree = deg, prec = _precision(a, b, c, d, e))
+            $af(res, a, b, deg + 1)
+            $af(res, res, c, deg + 1)
+            $af(res, res, d, deg + 1)
+            $af(res, res, e, deg + 1)
             return res
         end
     end
