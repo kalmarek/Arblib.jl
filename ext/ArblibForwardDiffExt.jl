f0661f11a2cfc231b23c717abab72f27d8981f1e
Joel Dahne <joel@dahne.eu>
2026-06-30 08:14:31 +0200

    Formatting

--- ext/ArblibForwardDiffExt.jl
+++ ext/ArblibForwardDiffExt.jl
@@ -3,8 +3,8 @@ module ArblibForwardDiffExt
 using Arblib: Arb
 using ForwardDiff: Dual
 
-function Base.promote_rule(::Type{Arb}, ::Type{Dual{T, V, N}}) where {T, V, N}
-    return Dual{T, promote_type(Arb, V), N}
+function Base.promote_rule(::Type{Arb}, ::Type{Dual{T,V,N}}) where {T,V,N}
+    return Dual{T,promote_type(Arb, V),N}
 end
 
 end # module
